256158f2bc7858a86c6c5d9f5535e22ae420de63
DaniSan <d.santosc@terra.com.br>
2022-02-13 16:14:21 -0300

    Estilizacao da Transaction Table terminada

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "./style";
 import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
 
 export const Dashboard = () => {
     return (
         <Container>
             <Summary />
+            <TransactionsTable/>
         </Container>
     );
 };
